016e163144590cf0e1d26f1383636dd055c1ae6b
Ankita <Ankita@LAPTOP-80FTJLO3>
2024-01-01 19:54:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace List_and_Dictionary_Assingment
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+
+//            myDictionary.Add("Apple", 10);
+//            myDictionary.Add("Banana", 5);
+//            myDictionary.Add("Orange", 8);
+
+
+//            Console.WriteLine("Number of Apples: " + myDictionary["Apple"]);
+//            Console.WriteLine("Number of Bananas: " + myDictionary["Banana"]);
+//            Console.WriteLine("Number of Oranges: " + myDictionary["Orange"]);
+
+
+//            if (myDictionary.ContainsKey("Grapes"))
+//            {
+//                Console.WriteLine("Number of Grapes: " + myDictionary["Grapes"]);
+//            }
+//            else
+//            {
+//                Console.WriteLine("Grapes are not in the dictionary.");
+//            }
+
+
+//            Console.WriteLine("\nAll Fruits in Dictionary:");
+//            foreach (var fruits in myDictionary)
+//            {
+//                Console.WriteLine(fruits.Key + ": " + fruits.Value);
+//            }
+//            Console.ReadLine();
+//        }
+
+
+//    }
+//}
---
+++ Properties/List Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_and_Dictionary_Assingment.Properties
+{
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public double Price { get; set; }
+        }
+
+    class CarDealership
+    {
+        static void Main()
+        {
+            List<Car> inventory = new List<Car>();
+
+            
+            inventory.Add(new Car { Make = "Toyota", Model = "Camry", Year = 2022, Price = 25000 });
+            inventory.Add(new Car { Make = "Honda", Model = "Accord", Year = 2023, Price = 27000 });
+            inventory.Add(new Car { Make = "Ford", Model = "Mustang", Year = 2021, Price = 35000 });
+
+            
+            Console.WriteLine("Cars in the inventory:");
+            foreach (Car car in inventory)
+            {
+                Console.WriteLine($"{car.Year} {car.Make} {car.Model} - ${car.Price}");
+
+            }
+
+            Console.ReadLine();
+        
+}   }   }
